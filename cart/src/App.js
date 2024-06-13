aeb31c60bc1057c93ef85e8771f21b3bb89c95bf
Raushan Kumar Thakur <raushankumarthakur311@gmail.com>
2024-06-13 15:29:30 +0530

    Make button functional

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,29 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-
+import React, { useState } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
 
 function App() {
-  const productList = [
+  const products = [
     {
       price: 33400,
       name: "Vivo V 21 5G",
-      quantity: 0
+      quantity: 0,
     },
     {
       price: 14999,
       name: "Redmi Note 7 pro",
-      quantity: 0
-    }
-  ]
+      quantity: 0,
+    },
+  ];
+
+  let [productList, setProductList] = useState(products);
+
+  const incrementQuantity = (index) => {
+    let newProductList = [...productList];
+    newProductList[index].quantity++;
+    setProductList(newProductList);
+  };
+
+  const decrementQuantity = (index) => {
+    let newProductList = [...productList];
+    newProductList[index].quantity > 0
+      ? newProductList[index].quantity--
+      : (newProductList[index].quantity = 0);
+    setProductList(newProductList);
+  };
+
   return (
     <>
-    <Navbar/>
-    <main className='container mt-5'>
-      <ProductList productList ={productList}/>
-    </main>
-    {/* <Footer/> */}
+      <Navbar />
+      <main className="container mt-5">
+        <ProductList
+          productList={productList}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+        />
+      </main>
+      {/* <Footer/> */}
     </>
   );
 }
